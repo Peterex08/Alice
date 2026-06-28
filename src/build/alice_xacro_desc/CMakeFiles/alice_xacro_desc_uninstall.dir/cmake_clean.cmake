a664c7fd393ff554cd8f2af9d3b696615eadb82d
Pedro Afonso <hshaggggiiutjj@gmail.com>
2026-06-28 19:38:19 -0300

    carried to ubuntu

--- src/build/alice_xacro_desc/CMakeFiles/alice_xacro_desc_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/alice_xacro_desc_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/alice_xacro_desc_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
